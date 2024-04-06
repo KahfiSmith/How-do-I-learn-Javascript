fea3189a853a42544490217e8d5af23be418e8b9
KahfiSmith <alkahfii2018@gmail.com>
2024-04-06 13:33:54 +0700

    pertama

---
+++ DOM/script.js
@@ -0,0 +1,12 @@
+// alert
+let message = 'Selamat Datang';
+let nama = ' Kahfi';
+alert(message + nama);
+
+// console
+// let name = prompt('Masukkan nama kamu', 'John Doe');
+// console.log(name);
+// console.log('Ini adalah console log');
+// console.info('Ini adalah console info');
+// console.warn('Ini adalah console warn');
+// console.error('Ini adalah console error');
