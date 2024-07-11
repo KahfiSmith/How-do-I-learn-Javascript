29bf96d903ec5c689ac973afb8547325fee7c0f4
KahfiSmith <alkahfii2018@gmail.com>
2024-07-11 22:52:27 +0700

    konversi string dan NaN

--- Fundamental JavaScript/konversi.js
+++ Fundamental JavaScript/konversi.js
@@ -6,3 +6,21 @@ const a = 1;
 const b = 3;
 const total = a.toString() + b.toString();
 console.log(total);
+
+console.log(`${parseInt("1salah")}`);
+console.log(`${parseFloat("1salahjuga")}`);
+
+console.log(`${Number("1salah")}`);
+console.log(`${Number("1salahjuga")}`);
+
+const first = Number('salah');
+const totalNumber = first + 100;
+console.log(`${totalNumber}`);
+
+console.log(`${isNaN(first)}`);
+console.log(`${isNaN(100)}`);
+console.log(`${isNaN(NaN)}`);
+
+
+
+
