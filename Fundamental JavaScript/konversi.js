67ca2736978230bc979b357bc7d8bb83ab3287aa
KahfiSmith <alkahfii2018@gmail.com>
2024-07-11 22:40:09 +0700

    konversi

---
+++ Fundamental JavaScript/konversi.js
@@ -0,0 +1,8 @@
+console.log(`${parseInt("1.9")}`);
+console.log(`${parseFloat("1.1")}`);
+console.log(`${Number("1.1")}`);
+
+const a = 1;
+const b = 3;
+const total = a.toString() + b.toString();
+console.log(total);
