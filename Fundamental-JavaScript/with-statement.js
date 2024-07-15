85017d8009849a8173b7b1eaf02ffb409d4dccc7
KahfiSmith <alkahfii2018@gmail.com>
2024-07-15 19:57:39 +0700

    with statement

---
+++ Fundamental-JavaScript/with-statement.js
@@ -0,0 +1,18 @@
+const person = {
+  firstname: "al",
+  middlename: "kahfi",
+  lastname: "kun",
+  age: 21,
+};
+
+console.log(person.firstname);
+console.log(person.middlename);
+console.log(person.lastname);
+console.log(person.age);
+
+// with (person) {
+//   console.log(firstname);
+//   console.log(middlename);
+//   console.log(lastname);
+//   console.log(age);
+// }
