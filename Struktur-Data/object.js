feb059b57a46b4905d5903696e6ef8955c64c259
KahfiSmith <alkahfii2018@gmail.com>
2024-07-26 20:53:44 +0700

    object + adding gitignore

--- Struktur-Data/object.js
+++ Struktur-Data/object.js
@@ -5,7 +5,7 @@ const me = {
   "is Genius": true,
 };
 
-console.log(`hi, nama saya ${me.nama}`)
+console.log(`hi, nama saya ${me.nama}`);
 console.log(`umur saya ${me.umur} tahun`);
 console.log(`hobi saya adalah ${me.hobi}`);
 console.log(`saya adalah seorang ${me["is Genius"]}`);
@@ -14,9 +14,38 @@ const spaceship = {
   name: "Millenium Falcon",
   manufacturer: "Corellian Engineering Corporation",
   maxSpeed: 1200,
-  color: "Light gray"
+  color: "Light gray",
 };
 
 spaceship.color = "Glossy red";
 spaceship.maxSpeed = 1300;
 console.log(spaceship);
+
+{
+  const target = {
+    firstname: "kahfi",
+  };
+  const source = {
+    lastname: "smith",
+  };
+
+  Object.assign(target, source);
+  console.log(target);
+  console.log(source);
+}
+
+{
+  const person = {
+    firstname: "kahfi",
+    lastname: "smith",
+  };
+
+  // tidak bisa diubah dan dihapus
+  Object.freeze(person);
+  // tidak bisa dihapus
+  Object.seal(person);
+
+  person.firstname = "miselia";
+  delete person.lastname;
+  console.log(person);
+}
