588719c292785fcba154faad4cf3798a13ac4684
KahfiSmith <alkahfii2018@gmail.com>
2024-07-27 23:07:40 +0700

    json, date, math

--- Struktur-Data/date.js
+++ Struktur-Data/date.js
@@ -13,13 +13,13 @@
 {
   // epoch and unix timestamp
   const date1 = new Date();
-  console.log(date1.getTime);
+  console.log(date1.getTime());
 
   const date2 = new Date(2020, 10, 10);
-  console.log(date2.getTime);
+  console.log(date2.getTime());
 
   const date3 = new Date(2020, 10, 10, 1, 0, 0, 0);
-  console.log(date3.getTime);
+  console.log(date3.getTime());
 
   console.log(Date.now());
 }
@@ -31,4 +31,18 @@
 
   const date = new Date(time);
   console.log(date);
+}
+
+{
+  // setter and getter date
+  const date = new Date();
+
+  console.log(date.getFullYear());
+  console.log(date.getMonth());
+  console.log(date.getDate());
+  console.log(date.getHours());
+  console.log(date.getMinutes());
+  console.log(date.getSeconds());
+  console.log(date.getMilliseconds());
+  console.log(date.getTimezoneOffset());
 }
---
+++ Struktur-Data/math.js
@@ -0,0 +1,6 @@
+console.log(Math.max(10, 12, 14, 19, 38))
+console.log(Math.min(10, 12, 14, 19, 38))
+console.log(Math.round(10.8))
+console.log(Math.round(10.1))
+console.log(Math.floor(10.8))
+console.log(Math.ceil(10.2))
