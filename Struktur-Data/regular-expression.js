05aa2853e09f58e72e0b94ae19ba653423c30179
KahfiSmith <alkahfii2018@gmail.com>
2024-08-03 23:09:29 +0700

    regex part 1

---
+++ Struktur-Data/regular-expression.js
@@ -0,0 +1,13 @@
+{
+  const regex1 = /kahfi/;
+  const regex2 = new RegExp("kahfi");
+  const regex3 = new RegExp("al");
+}
+
+{
+    const name = 'kahfi smith'
+    const regex = /kahfi/
+
+    const result = regex.exec(name)
+    console.log(result)
+}
